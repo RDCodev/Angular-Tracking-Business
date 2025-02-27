c124bd5ab2edc3efce627f8f7afa07921f6a0b41
IIRDCodeII <rdcode.dev@gmail.com>
2025-02-26 22:33:51 -0500

    Refactor app component: remove NxWelcomeComponent and update routing configuration; add async animations provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [RouterModule],
 })
 export class AppComponent {
-  title = 'moderng-ui';
+
 }
