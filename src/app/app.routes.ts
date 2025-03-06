8a843a218e05bcaacca6339cbf76f97ce790fb08
Stalin D. Pillajo <StalinP@andelytica.com>
2025-03-06 11:34:46 -0500

    feat: Add profile route and update access route title in app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,11 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: 'access',
-    title: 'access',
+    title: 'Access',
     loadComponent: () => import('@features/access/access.component').then(mod => mod.AccessComponent),
+  }, {
+    path: 'profile',
+    title: 'Profile',
+    loadComponent: () => import('@features/profile/profile.component').then(mod => mod.ProfileComponent)
   }
 ];
