d94a4899a520ddc12fb3956720a691a5b3d850a3
Stalin D. Pillajo <StalinP@andelytica.com>
2025-03-07 16:25:34 -0500

    feat: Enhance SignIn component with validation messages and submit logic

--- src/app/features/access/login/components/sign-in/sign-in.component.ts
+++ src/app/features/access/login/components/sign-in/sign-in.component.ts
@@ -1,11 +1,15 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, EnvironmentInjector, inject, OnInit, runInInjectionContext, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
-import { AccessForm, FormCreator, SignInFormCreator } from '../../utils/access-factory.util';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { AccessForm, FormCreator, SignInFormCreator } from '@features/access/login/utils/access-factory.util';
+import { SupabaseService } from '@core/services/supabase.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -18,19 +22,40 @@ import { AccessForm, FormCreator, SignInFormCreator } from '../../utils/access-f
     MatInputModule,
     MatCheckboxModule,
     MatTabsModule,
+    MatProgressBarModule,
     ReactiveFormsModule,
+    MatIconModule
   ],
 })
 export class SignInComponent implements OnInit {
 
-  public accessForm!: AccessForm;
+  private readonly envInjector = inject(EnvironmentInjector);
+  private readonly _supabase = inject(SupabaseService);
 
-  get signInForm(): FormGroup {
-    return this.accessForm.form;
+  private accessForm!: AccessForm;
+  
+  public signInForm!: FormGroup;
+  public singInSubmit!: any;
+  public signInFormChanges!: Signal<void>;
+
+  constructor() { 
+    effect(() => {
+      console.log('[Effect] - ', this.signInFormChanges());
+    });
+
+    console.log(this._supabase);
+  }
+  
+  ngOnInit(): void {    
+    this.initializeForm();
   }
 
-  ngOnInit(): void {
-    this.initializeForm();
+  get username() {
+    return this.signInForm.controls['username'];
+  }
+
+  get password() {
+    return this.signInForm.controls['password'];
   }
 
   private initializeForm() {
@@ -38,6 +63,16 @@ export class SignInComponent implements OnInit {
   }
 
   private initAccessForm(creator: FormCreator) {
-    this.accessForm = creator.createForm();
+    runInInjectionContext(this.envInjector, () => {
+      this.accessForm = creator.createForm();
+
+      this.signInForm = this.accessForm.form;
+      this.signInFormChanges = toSignal(this.signInForm.valueChanges);
+
+    });
+  }
+
+  public submit() {
+    this.accessForm.validate((param: any) => console.log('[Submit] - ', param));
   }
 }
