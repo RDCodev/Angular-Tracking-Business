d6aabe246c0fba542af5926155012c4688ca4b5b
IIRDCodeII <rdcode.dev@gmail.com>
2025-03-07 22:11:54 -0500

    feat: Enhance SignUp component layout and add password visibility toggle

--- src/app/features/access/login/components/sign-up/sign-up.component.ts
+++ src/app/features/access/login/components/sign-up/sign-up.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 import { AccessForm, FormCreator, SignUpFormCreator } from '../../utils/access-factory.util';
 
 @Component({
@@ -19,26 +20,48 @@ import { AccessForm, FormCreator, SignUpFormCreator } from '../../utils/access-f
     MatCheckboxModule,
     MatTabsModule,
     ReactiveFormsModule,
+    MatIconModule
   ],
 })
 export class SignUpComponent implements OnInit {
 
-  public accessForm!: AccessForm;
+  private accessForm!: AccessForm;
 
-  get signUpForm(): FormGroup {
+  public hide = signal(true);
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initAccessForm(new SignUpFormCreator());
+  }
+
+  get signUpForm() {
     return this.accessForm.form;
   }
 
-  ngOnInit(): void {
-    this.initializeForm();
+  get firstName() {
+    return this.signUpForm.controls["firstName"];
   }
 
-  private initializeForm() {
-    this.initAccessForm(new SignUpFormCreator());
+  get lastName() {
+    return this.signUpForm.controls["lastName"];
+  }
+
+  get username() {
+    return this.signUpForm.controls["username"];
+  }
+
+  get password() {
+    return this.signUpForm.controls["password"];
   }
 
   private initAccessForm(creator: FormCreator) {
     this.accessForm = creator.createForm();
+  }
+
+  public onHidePassword(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
   }
 
 }
