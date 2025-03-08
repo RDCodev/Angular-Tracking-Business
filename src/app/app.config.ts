8cf471ba59395ae7c0e555a684532b22aedb1067
IIRDCodeII <rdcode.dev@gmail.com>
2025-03-07 21:22:22 -0500

    feat: Remove zone.js dependency and update app configuration for zoneless change detection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes), provideAnimationsAsync(),
+    provideExperimentalZonelessChangeDetection(),
+    provideHttpClient(withFetch()),
+    provideRouter(appRoutes), 
+    provideAnimationsAsync(),
   ],
 };
