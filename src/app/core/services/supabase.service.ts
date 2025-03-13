01a4070b3b73676e74081d1f560a7de35143a83a
IIRDCodeII <rdcode.dev@gmail.com>
2025-03-12 21:57:16 -0500

    feat: Refactor SupabaseService for improved session handling and user creation

--- src/app/core/services/supabase.service.ts
+++ src/app/core/services/supabase.service.ts
@@ -2,44 +2,48 @@ import { inject, Injectable } from "@angular/core";
 import { SignUpDTO } from "@features/access/login/models/sign-up.model";
 import { AuthSession, createClient, SupabaseClient } from "@supabase/supabase-js";
 
+const NG_APP_SUPABASE_ANON_PUBLIC_KEY = import.meta.env.NG_APP_SUPABASE_ANON_PUBLIC_KEY
+const NG_APP_SUPABASE_URL             = import.meta.env.NG_APP_SUPABASE_URL
+
 @Injectable({
   providedIn: 'root',
 })
 export class SupabaseService {
-  static readonly SUPABASE_URL = import.meta.env.NG_APP_SUPABASE_URL;
-  static readonly SUPABASE_ANON_PUBLIC_KEY = import.meta.env
-    .NG_APP_SUPABASE_ANON_PUBLIC_KEY;
 
-  public _session: AuthSession | null = null;
+  private readonly SUPABASE_URL             = NG_APP_SUPABASE_URL;
+  private readonly SUPABASE_ANON_PUBLIC_KEY = NG_APP_SUPABASE_ANON_PUBLIC_KEY;
+
+  private _session: AuthSession | null = null;
+
   public supabase!: SupabaseClient;
 
   constructor() {
-    this.supabase = createClient(
-      SupabaseService.SUPABASE_URL,
-      SupabaseService.SUPABASE_ANON_PUBLIC_KEY
-    );
+    this.supabase = createClient(this.SUPABASE_URL, this.SUPABASE_ANON_PUBLIC_KEY);
   }
 
   get session() {
-    if (!this._session) {
-      this.initSupabaseSession();
-    }
+
+    if (!this._session) this.initSupabaseSession();
 
     return this._session;
   }
 
   private async initSupabaseSession() {
-    const {
-      data: { session },
+
+    const { 
+      data: { session }, 
+      error 
     } = await this.supabase.auth.getSession();
 
     this._session = session;
   }
 
-  private async createUser(payload: SignUpDTO) {
-    await this.supabase.auth.signUp({
-      email: payload.username,
-      password: payload.password
-    })
+  public async createUser(signInDTO: SignUpDTO) {
+    const { 
+      data: { session, user }, 
+      error 
+    } = await this.supabase.auth.signUp({ ...signInDTO });
+
+    this._session = session;
   }
 }
