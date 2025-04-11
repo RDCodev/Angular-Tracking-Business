eaff8965aebeedba85b572e5537336b91a3df4fa
IIRDCodeII <rdcode.dev@gmail.com>
2025-04-10 20:38:02 -0500

    feat: Implement ProfileComponent layout and structure; integrate MainLayout for improved design

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { MainLayout } from "@layouts/home/main.component";
 
 @Component({
     selector: 'kmx-profile',
     templateUrl: './profile.component.html',
     styleUrls: ['./profile.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        MainLayout
+    ]
 })
 export class ProfileComponent { }
